4e3559a0328c03566c3326e9eba5c2167415829a
Daniel Touriño <contacto@dannieltp.dev>
2025-12-28 00:40:16 +0100

    feat(ingest-api): add dotenv configuration for environment variables

--- packages/ingest-api/src/main.ts
+++ packages/ingest-api/src/main.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './routes/app/app.module';
 import { setupBullBoard } from 'routes/queue/bull-board';
