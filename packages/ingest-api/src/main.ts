445023796c294e55fd881026868b49a6eabfac5e
Daniel Touriño <contacto@dannieltp.dev>
2025-12-28 17:24:49 +0100

    refactor(ingest-api): remove bull board setup and related dependencies

--- packages/ingest-api/src/main.ts
+++ packages/ingest-api/src/main.ts
@@ -2,13 +2,11 @@ import 'dotenv/config'
 
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './routes/app/app.module'
-import { setupBullBoard } from 'queue/bull-board'
 import { AppDataSource } from 'db/data-source'
-;(async () => {
+
+(async () => {
     await AppDataSource.initialize()
 
     const app = await NestFactory.create(AppModule)
-    setupBullBoard(app.getHttpAdapter().getInstance())
-
     await app.listen(process.env.PORT ?? 4001)
 })()
